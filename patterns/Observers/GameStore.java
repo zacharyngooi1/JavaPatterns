571b2ee007adbbbe5be47f8ba421af9c8e5fbda6
Zach <47660097+zacharyngooi1@users.noreply.github.com>
2020-08-17 19:55:04 +1000

    Add files via upload

--- patterns/Observers/GameStore.java
+++ patterns/Observers/GameStore.java
@@ -1,10 +1,12 @@
 public class GameStore implements Observer {
     String name;
     Game game;
+    int gameprice;
 
     GameStore(String NewName, Game newGame) {
-        this.game = newGame;
         this.name = NewName;
+        this.game = newGame;
+        this.gameprice = newGame.getPrice();
     }
 
 	public String getName() {
@@ -25,8 +27,17 @@ public class GameStore implements Observer {
 
 	@Override
 	public void update(Game game) {
-        System.out.println(this.getName() + " has updated " + this.game.getName() +" With a new price of " + game.getPrice());
         setGame(game);
+        this.gameprice = game.getPrice();
+        System.out.println(this.getName() + " has updated " + this.game.getName() +" With a new price of " + this.getGameprice());
+    }
+
+	public int getGameprice() {
+		return gameprice;
+	}
+
+	public void setGameprice(int gameprice) {
+		this.gameprice = gameprice;
 	}
 
     
--- patterns/Observers/Gametest.java
+++ patterns/Observers/Gametest.java
@@ -4,13 +4,13 @@ public class Gametest {
         Game mario = new MarioKart("mariokart1", 30);
         GameStore JeffsStore = new GameStore("JeffStore", mario);
         GameStore BobsStore = new GameStore("BobsStore", mario);
-        mario.attach(JeffsStore);
         mario.attach(BobsStore);
+        mario.attach(JeffsStore);
         mario.setPrice(99);
         mario.detach(BobsStore);
         mario.setPrice(2020);
-        System.out.println(JeffsStore.getGame().getPrice());
+        System.out.println(JeffsStore.getGameprice());
         // Are observers supposed to still retain the observable's updated values?
-        System.out.println(BobsStore.getGame().getPrice());
+        System.out.println(BobsStore.getGameprice());
         }
 }
